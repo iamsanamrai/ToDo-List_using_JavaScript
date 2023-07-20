3208fb3f7bfadfb9206013594826c3727f1e07c2
sawnum <lonelykan6a@gmail.com>
2023-07-20 23:43:51 +0530

    javascript modified

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#todoInput");
-    const list_el = document.querySelector("#tasks");
+function addTodo() {
+    var input = document.getElementById("todoInput");
+    var task = input.value;
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    if (!task) {
+        alert("Please write some task.");
+        return;
+    }
 
-        const task = input.value;
+    if (task !== '') {
+      const todoItem = document.createElement("div");
+      todoItem.classList.add("tasks");
+  
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("change", function() {
+        todoItem.classList.toggle("tasks");
+      });
 
-        if  (!task) {
-              alert("Please fill out the task");
-              return;
-        } 
-
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("contents");
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type = "checkbox";
-        task_input_el.value = "text";
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el)
-
-        const task_action_el = document.createElement("div");
-        task_action_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
-        
-        task_action_el.appendChild(task_edit_el);
-        task_action_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_action_el);
-
-        list_el.appendChild(task_el);
-        input.value = "";
-        
-    })
-})
+      todoItem.appendChild(checkbox);
+  
+      const label = document.createElement("label");
+      label.appendChild(document.createTextNode(task));
+      todoItem.appendChild(label);
+  
+      document.getElementById("tasks").appendChild(todoItem);
+      input.value = "";
+    }
+  }
+  
